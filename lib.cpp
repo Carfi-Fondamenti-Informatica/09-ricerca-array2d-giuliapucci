79265fadf7ea3586f5b27bf80ffe3abbd164e9d5
giuliapucci <115046420+giuliapucci@users.noreply.github.com>
2022-12-17 09:37:53 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,10 @@
-
+int bodyguard (char lista[][20], char nome[20]) {
+  int i=0, j=0;
+  while ((j>29)and(i<10)) {
+    if (lista[i][j]!=nome[j]) {
+      i++;
+      j=0;
+    } else if (lista[i][j]==nome[j]) {
+      j++;
+    }
+  } return 1;
